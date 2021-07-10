fb8e3db1abce22fe96e48636992293992203ac24
home4lab <87222176+home4lab@users.noreply.github.com>
2021-07-10 18:56:19 +0700

    Update install-docker-ubuntu.sh

--- install-docker-ubuntu.sh
+++ install-docker-ubuntu.sh
@@ -1,4 +1,4 @@
-sudo apt install \
+apt install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -7,11 +7,11 @@ sudo apt install \
     
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo apt-key fingerprint 0EBFCD88
+apt-key fingerprint 0EBFCD88
 
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
    
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+apt-get install docker-ce docker-ce-cli containerd.io -y
