b3dfd047d7ab1aad0655fc6be9332367a230c47e
home4lab <87222176+home4lab@users.noreply.github.com>
2021-07-10 14:20:03 +0700

    Update install-acunetix.sh

--- install-acunetix.sh
+++ install-acunetix.sh
@@ -2,3 +2,10 @@ docker pull mihonho/acunetix_13
 
 docker run --privileged=true --name acunetix -p3443:3443 -itd mihonho/acunetix_13 /sbin/init
 
+
+echo " Username: admin@admin.com "
+echo " Password: admin@123 "
+echo " Path: /home/acunetix/.acunetix/ "
+echo " User: acunetix Pass: acunetix "
+
+
