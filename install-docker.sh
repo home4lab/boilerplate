85b2fa995f93705671f29c247580525d1bdf1677
home4lab <87222176+home4lab@users.noreply.github.com>
2021-07-10 14:08:18 +0700

    Update and rename docker.sh to install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,4 +1,4 @@
-sudo apt-get install \
+sudo apt install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -6,7 +6,9 @@ sudo apt-get install \
     software-properties-common
     
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 sudo apt-key fingerprint 0EBFCD88
+
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
