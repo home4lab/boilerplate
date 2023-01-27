eed62f8f4df8788d8cbdfddd5c6eb723c322bbf9
home4lab <87222176+home4lab@users.noreply.github.com>
2023-01-27 15:21:25 +0700

    Create download.sh

---
+++ container/passbolt/download.sh
@@ -0,0 +1,14 @@
+wget https://download.passbolt.com/ce/docker/docker-compose-ce.yaml
+
+wget https://github.com/passbolt/passbolt_docker/releases/latest/download/docker-compose-ce-SHA512SUM.txt
+
+sha512sum -c docker-compose-ce-SHA512SUM.txt
+
+docker-compose -f docker-compose-ce.yaml up -d
+
+echo docker-compose -f docker-compose-ce.yaml exec passbolt su -m -c "/usr/share/php/passbolt/bin/cake \
+                                passbolt register_user \
+                                -u <your@email.com> \
+                                -f <yourname> \
+                                -l <surname> \
+                                -r admin" -s /bin/sh www-data
