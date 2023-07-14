574729070d7c05cb1434c946beabcfd509f39fe4
home4lab <87222176+home4lab@users.noreply.github.com>
2023-07-14 16:24:07 +0700

    Update install-docker-ubuntu.sh

--- script/install-docker-ubuntu.sh
+++ script/install-docker-ubuntu.sh
@@ -1,13 +1,8 @@
 sudo apt-get update -y
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release -y
 
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
 
 sudo mkdir -m 0755 -p /etc/apt/keyrings
-
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
